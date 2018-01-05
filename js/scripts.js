8b316b426a2d596e41909a689e50beb7140c3db6
Damian-Czarnota <31064067+Damian-Czarnota@users.noreply.github.com>
2018-01-05 22:34:17 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,23 +13,6 @@ $('html, body').animate({scrollTop: target.offset().top - 120}, 1000);
 });
 });
 
-jQuery( document ).ready(function() {
-    jQuery('#buttonForm').on('click',function(e){
-        e.preventDefault();
-        var empty = false;
-        jQuery('input:text'&&'textarea').each(function(){
-            if(jQuery(this).val()==''){
-                alert("Proszę wypełnić wymagane pola");
-            } else empty = true;
-        });
-        if(empty)
-            jQuery('#myModal').modal('toggle');
-        else
-            console.log('your error message');
-    }); 
-});
-
-
 $(window).scroll(function () {
     showText()
 });
